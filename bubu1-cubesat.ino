35d30e5b173c287593198b6ae0ce4940bb4d2434
0x7s0lt1 <zsoltfehervari@gmx.com>
2024-10-24 22:14:51 +0200

    asdf

--- bubu1-cubesat.ino
+++ bubu1-cubesat.ino
@@ -3,7 +3,7 @@
 // #include <SPI.h>             // Arduino SPI library
 
 #include "bitmap/pooolse_map.h"
-#include "bitmap/dp.h"
+//#include "bitmap/dp.h"
 
 // ST7789 TFT module connections
 #define TFT_CS    10
@@ -13,8 +13,7 @@
 Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 
 const unsigned char* epd_bitmap_poolse_arrays[] = {
-    epd_bitmap_poolse,
-    epd_bitmap_dp
+    epd_bitmap_poolse
 };
 
 int i = 0;
@@ -23,22 +22,52 @@ void setup() {
 	tft.init(240, 240, SPI_MODE2);
   tft.setRotation(2);
   tft.setTextWrap(false);
+
+  tft.setTextColor(ST77XX_BLACK, ST77XX_WHITE);
 }
 
 void loop() {
 
   tft.fillScreen(ST77XX_BLACK);
 
-  if(i == 2){
+  if(i == 1){
 
+  tft.setTextSize(5);
   tft.setCursor(20, 80);
-  tft.setTextColor(ST77XX_BLACK, ST77XX_WHITE);
-  tft.setTextSize(4);
-  tft.print(" BUBU 1");
+  tft.print("BUBU-1");
 
-  i = 0;
+  i++;
 
-  }else{
+  }
+  else if(i == 2){
+
+    tft.setTextSize(7);
+    tft.setCursor(20, 40);
+    tft.print("DONT");
+    tft.setCursor(20, 120);
+    tft.print("PANIC");
+
+
+    i++;
+  }
+  else if(i == 3){
+
+    tft.setTextSize(3);
+    tft.setCursor(10, 20);
+    tft.print("Luck is");
+    tft.setCursor(10, 60);
+    tft.print("when");
+    tft.setCursor(10, 100);
+    tft.print("preparation");
+    tft.setCursor(10, 140);
+    tft.print("meets");
+    tft.setCursor(10, 180);
+    tft.print("opportunity.");
+    //Luck is when preparation meets opportunity
+
+    i = 0;
+  }
+  else{
 
     tft.drawBitmap(0, 0, epd_bitmap_poolse_arrays[i], 240, 240, ST77XX_WHITE);
     i++;
